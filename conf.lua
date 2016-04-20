e3f01b65346485bae1f7875529ccb27bd3306eef
Nixola <niki.or97@yahoo.it>
2016-04-20 18:04:57 +0200

    Fixed version

--- conf.lua
+++ conf.lua
@@ -2,11 +2,11 @@
 
 function love.conf(t)
     t.title = "LUA Tower Defense"
-    t.version = "0.9.2"
+    t.version = "0.10.1"
     t.window.width = 1280
     t.window.height = 900
     t.console = true
-    t.window.vsync = false
+    t.window.vsync = true
 
     t.window.icon = "res/icon.png"
 
